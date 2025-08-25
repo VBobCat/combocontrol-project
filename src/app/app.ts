5b8ca9437acafc6abbe5553ffc4acc5dd38b6ebc
Antonio Carlos Jorge Patricio <antonio.patricio@agu.gov.br>
2025-08-25 17:28:35 -0300

    Implementando

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, effect, inject, OnInit, signal} from '@angular/core';
+import {ComboControlComponent} from '../combo-control/combo-control.component';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {HttpClient} from '@angular/common/http';
+import {MatFormField} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ComboControlComponent, MatFormField],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('combocontrol-project');
+  private readonly http = inject(HttpClient);
+  datasource = toSignal(
+    this.http.get<any[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados/35/municipios'),
+    {initialValue: []}
+  );
+
+  constructor() {
+  }
 }
--- src/combo-control/combo-control.component.ts
+++ src/combo-control/combo-control.component.ts
@@ -1,30 +1,67 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatInputModule } from '@angular/material/input';
-import { debounceTime, distinctUntilChanged, map } from 'rxjs';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  effect,
+  forwardRef,
+  input,
+  Optional,
+  Self,
+  signal, viewChild
+} from '@angular/core';
+import {takeUntilDestroyed, toSignal} from '@angular/core/rxjs-interop';
+import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, NgControl, ReactiveFormsModule} from '@angular/forms';
+import {MatAutocomplete, MatAutocompleteModule, MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {debounceTime, distinctUntilChanged, filter, map, tap} from 'rxjs';
 
 @Component({
   selector: 'app-combo-control',
   imports: [MatAutocompleteModule, MatInputModule, ReactiveFormsModule,
   ],
-  template: `<input matInput [formControl]="fcInput">
-  <mat-autocomplete #auto="matAutocomplete">
+  template: `<input
+    (blur)="onBlur($event)"
+    [formControl]="fcInput"
+    [matAutocomplete]="auto"
+    matInput
+  >
+  <mat-autocomplete
+    #auto="matAutocomplete"
+    (optionSelected)="onChange($event.option.value)"
+    [displayWith]="displayFn()"
+  >
     @for (option of filteredlist(); track option.value) {
-      <mat-option [value]="option.value">{{option.display}}</mat-option>
+      <mat-option [value]="option.value">{{ option.display }}</mat-option>
     }
   </mat-autocomplete>`,
   styles: '',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{provide: NG_VALUE_ACCESSOR, useExisting: ComboControlComponent, multi: true}]
 })
-export class ComboControlComponent {
+export class ComboControlComponent implements ControlValueAccessor {
+
+  fcInput = new FormControl('', {nonNullable: true});
+
+  fcSelect = new FormControl<any>(null);
+
   datasource = input<any[]>([]);
+
+  disabledState = signal<boolean>(false);
+
   display = input<null | string | ((o: any) => string)>(null);
+
   evaluate = input<null | string | ((o: any) => any)>(null);
-  ascii = (s: string) => s.toLocaleLowerCase()
-    .replaceAll(/áâàäã/gu, 'a').replaceAll(/ç/gu, 'c').replaceAll(/éêèë/gu, 'e').replaceAll(/íîìï/gu, 'i')
-    .replaceAll(/ñ/gu, 'n').replaceAll(/óôòöõ/gu, 'o').replaceAll(/úûùü/gu, 'u').replaceAll(/\W+/gu, ' ')
+
+  ascii = (s: string) => s.toLowerCase()
+    .replaceAll(/[áâàäã]/gu, 'a')
+    .replaceAll(/ç/gu, 'c')
+    .replaceAll(/[éêèë]/gu, 'e')
+    .replaceAll(/[íîìï]/gu, 'i')
+    .replaceAll(/ñ/gu, 'n')
+    .replaceAll(/[óôòöõ]/gu, 'o')
+    .replaceAll(/[úûùü]/gu, 'u')
+    .replaceAll(/[^\p{L}\p{N}]+/gu, ' ')
+
   listsource = computed(() => {
     const displayer = this.display();
     const displaylist = typeof displayer === 'string' ? this.datasource().map(v => String(v[displayer]))
@@ -34,12 +71,67 @@ export class ComboControlComponent {
     const valuelist = typeof evaluator === 'string' ? this.datasource().map(v => v[evaluator])
       : typeof evaluator === 'function' ? this.datasource().map(v => evaluator(v))
         : this.datasource().map(v => v);
-    return displaylist.map((display, i) => ({ display, ascii: this.ascii(display), value: valuelist[i] }))
+    return displaylist.map((display, i) => ({display, ascii: this.ascii(display).split(/\s+/gu), value: valuelist[i]}))
   });
-  fcInput = new FormControl('', { nonNullable: true });
-  inputAscii = toSignal(this.fcInput.valueChanges.pipe(map(v => this.ascii(v)), debounceTime(500), distinctUntilChanged(), map(v => v.split(' '))), { initialValue: [] });
+
+  inputAscii = toSignal(
+    this.fcInput.valueChanges.pipe(
+      filter(v => typeof (v as unknown) === 'string'),
+      debounceTime(500),
+      distinctUntilChanged(),
+      tap(v => console.dir({texto: v})),
+      map(v => this.ascii(v)),
+    ), {initialValue: ''});
+
   filteredlist = computed(() => {
-    const inputAscii = this.inputAscii();
-    return this.listsource().filter(item => inputAscii.every(w => item.ascii.includes(w)));
+    const inputAsciiWords = this.inputAscii().split(/\s+/gu);
+    return this.listsource()
+      .filter(item => inputAsciiWords
+        .every(inputWord => item.ascii
+          .some(word => word.startsWith(inputWord))));
   });
+
+  displayFn = computed(() => {
+    const list = this.filteredlist();
+    return (value: any) => list.find(item => item.value === value)?.display ?? '';
+  })
+
+  onChange = (v: any) => {
+  };
+
+  onTouched = () => {
+  };
+
+  writeValue(obj: any): void {
+    const valueItem = this.listsource().find(item => item.value === obj);
+    this.fcInput.setValue(valueItem?.display ?? '');
+    this.fcSelect.setValue(valueItem?.value ?? null);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabledState.set(isDisabled);
+  }
+
+  constructor() {
+    this.fcSelect.valueChanges.pipe(takeUntilDestroyed(), tap(v => console.log('value =', v))).subscribe(v => this.onChange && this.onChange(v));
+    effect(() => {
+      if (this.disabledState()) this.fcInput.disable(); else this.fcInput.enable();
+    })
+  }
+
+  onBlur(_: FocusEvent) {
+    if (this.onTouched) this.onTouched();
+  }
+
+  optionSelected(matAutocompleteSelectedEvent: MatAutocompleteSelectedEvent) {
+    console.dir(matAutocompleteSelectedEvent);
+  }
 }
